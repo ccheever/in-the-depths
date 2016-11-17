043c4472ca2ff5e23c90e73c478db29e99e1e955
Charlie Cheever <ccheever@gmail.com>
2016-11-17 12:40:26 -0800

    Adding files

---
+++ Assets/index.js
@@ -0,0 +1,17 @@
+import Exponent from 'exponent';
+
+// We will refer to assets by a 'friendly name' such as 'splash-sound' or
+// 'player-sprite', offering an additional level of indirection over the
+// actual file paths.
+
+// Map of asset names to modules. List your assets here.
+const modules = {
+  'player-sprite': require('./avatar2.png'),
+  'aquarium': require('./Aquarium.png'),
+  'shark': require('./Shark.png'),
+  'diver': require('./Diver.png'),
+}
+
+// Export map of asset names to `Exponent.Asset` objects.
+export default Object.assign({}, ...Object.keys(modules).map((name) =>
+  ({ [name]: Exponent.Asset.fromModule(modules[name]) })));
